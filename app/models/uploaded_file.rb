687ca850e4c22e21e4327b8b4307417480d993ea
David Lesches <david@lesches.com>
2012-08-27 18:34:15 -0400

    Basic amazon s3 integration

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,3 +1,20 @@
 class UploadedFile < ActiveRecord::Base
-  attr_accessible :amazon_s3_id, :file_type, :name, :user_id
+
+  attr_accessor :file
+  attr_accessible :amazon_s3_id, :file_type, :name, :user_id, :file
+  belongs_to :user
+
+  after_create :upload_to_s3
+
+  def upload_to_s3
+    s3 = AWS::S3.new
+    b = s3.buckets[APP_CONFIG['amazon_bucket']]
+
+    # here we need to grab the basename of the uploaded file like 'thingamajig.txt'
+    # name = file
+
+    # below will be the amazon key. we need to pretty up this code a bit and append the file name, like 'thingamajig.txt'
+    amazon_s3_id = user.id.to_s << '-' << created_at.to_s.parameterize.gsub('-', '').gsub('utc', '') # << '-' << name
+    b.objects.create(amazon_s3_id, file)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
-  
-  attr_accessible :email, :first_name, :last_name, :password_digest, :password
-  
+
+  attr_accessible :email, :first_name, :last_name, :password_digest, :password, :password_confirmationU
+
   validates_presence_of :email, :password, :first_name, :last_name
-  
+
   has_secure_password
   has_many :transactions
   has_many :jobs
-  
+  has_many :uploaded_files
+
 end
